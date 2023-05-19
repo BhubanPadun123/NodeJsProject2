0248cdd8453aaf4af7a927773db460bae1dafc1d
Bhuban12345 <oterpadun1996@gmail.com>
2023-05-19 17:16:14 +0530

    middleware

--- myApp.js
+++ myApp.js
@@ -9,8 +9,13 @@ app.get("/",(req,res)=>{
     const absolutePath = __dirname+"/views/index.html"
     //console.log(absolutePath)
     //res.send("Hello Express")
+    // sendFile method is used to serve the html file.
     res.sendFile(absolutePath)
+
+    //serve the static file using the express.static. it's use to serve the static file
 })
+
+app.use(express.static(__dirname+"/public"))
 
 
 
