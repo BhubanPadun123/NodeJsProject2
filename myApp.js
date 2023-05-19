3bc2fabf95d65426c794adbcfadbcf4f4b6ac65f
Bhuban12345 <oterpadun1996@gmail.com>
2023-05-19 16:40:38 +0530

    serve html file

--- myApp.js
+++ myApp.js
@@ -2,9 +2,14 @@ let express = require('express');
 let app = express();
 
 
+
+
 console.log("Hello World")
 app.get("/",(req,res)=>{
+    const absolutePath = __dirname+"/views/index.html"
+    //console.log(absolutePath)
     res.send("Hello Express")
+    res.send(absolutePath)
 })
 
 
